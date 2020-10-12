37c4b078ff3e9b7ede5a3a00b7b28ac3b95e9e84
Lan Vo <vokhanhlan161@gmail.com>
2020-10-12 15:18:28 +0700

    update

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 low_ram='512144' #
 
-apt -y install gawk bc wget lsof
+sudo apt -y install gawk bc wget lsof
 
 cpu_name=$( awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo )
 cpu_cores=$( awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo )
@@ -70,11 +70,11 @@ confirm2 rm file
 #rm -f /etc/localtime
 #ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 
-apt install selinux-utils -y
+sudo apt install selinux-utils -y
 setenforce 0
 
-apt -y update
-apt -y upgrade
+sudo apt -y update
+# sudo apt -y upgrade
 
 ###clear
 printf "=========================================================================\n"
@@ -82,10 +82,10 @@ printf "Prepare done start setup server... \n"
 printf "=========================================================================\n"
 sleep 2
 
-apt install gnupg2 && apt update -y && apt upgrade -y && apt install zip -y && apt install -y libmagickwand-dev && apt install libmysqlclient-dev -y && apt install unzip -y && apt install git -y && git config --global user.name "ruby rails git" && git config --global user.email "ruby@localhost" && git config --list  && sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev -y && apt install nginx -y && sudo ufw allow 'Nginx HTTP' && systemctl start nginx && systemctl enable nginx && apt install nodejs -y && command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - && command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import - && curl -L get.rvm.io | bash -s stable && source /etc/profile.d/rvm.sh && rvm reload && rvm requirements run && rvm install ruby --latest && rvm use ruby --latest --default && ruby -v && gem install bundler && gem install rmagick && gem install rails --no-document && ruby -v && rails -v
+sudo apt install gnupg2 && sudo apt update -y && sudo apt upgrade -y && sudo apt install zip -y && sudo apt install -y libmagickwand-dev && sudo apt install libmysqlclient-dev -y && sudo apt install unzip -y && sudo apt install git -y && git config --global user.name "ruby rails git" && git config --global user.email "ruby@localhost" && git config --list  && sudo sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev -y && sudo apt install nginx -y && sudo ufw allow 'Nginx HTTP' && systemctl start nginx && systemctl enable nginx && sudo apt install nodejs -y && command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import - && command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import - && curl -L get.rvm.io | bash -s stable && source /etc/profile.d/rvm.sh && rvm reload && rvm requirements run && rvm install ruby --latest && rvm use ruby --latest --default && ruby -v && gem install bundler && gem install rmagick && gem install rails --no-document && ruby -v && rails -v
 
 # Install more
-apt -y install unzip zip nano openssl ntpdate
+sudo apt -y install unzip zip nano openssl ntpdate
 
 ntpdate asia.pool.ntp.org
 hwclock --systohc
@@ -96,7 +96,7 @@ printf "Config done ... \n"
 printf "=========================================================================\n"
 
 
-apt install ufw -y
+sudo apt install ufw -y
 sudo ufw allow ssh
 sudo ufw allow 22
 sudo ufw allow 80/tcp
